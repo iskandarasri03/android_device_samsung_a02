a48fc82f711444cb8ae05922ed63f5a84b1c05ed
iskandarasri03 <71484902+iskandarasri03@users.noreply.github.com>
2021-11-19 20:06:22 +0800

    Update and rename twrp_a02.mk to omni_a02.mk

--- omni_a02.mk
+++ omni_a02.mk
@@ -14,11 +14,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/samsung/a02/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a02
-PRODUCT_NAME := twrp_a02
+PRODUCT_NAME := omni_a02
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A022F
 PRODUCT_MANUFACTURER := samsung
