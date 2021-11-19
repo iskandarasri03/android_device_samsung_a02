d956dfc32892a12e43a5d69d24e5c6f4d0b00362
iskandarasri03 <71484902+iskandarasri03@users.noreply.github.com>
2021-11-19 15:29:10 +0800

    Update omni_a02.mk

--- omni_a02.mk
+++ omni_a02.mk
@@ -14,7 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/samsung/a02/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/common_full_phone.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
