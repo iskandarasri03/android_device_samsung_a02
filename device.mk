d4ea2703056f8b6dda28e82cdab61057f489824f
iskandarasri03 <71484902+iskandarasri03@users.noreply.github.com>
2021-11-20 19:01:16 +0800

    Add files via upload

---
+++ device.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/samsung/a02
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
